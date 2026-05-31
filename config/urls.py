6f6191a9c4682ede2cbfa45506017f9dcfe1816e
ABHISEK <abhisekthakur45@gmail.com>
2026-05-31 16:28:17 +0530

    Add Swagger routes and complete cookie authentication flow

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,45 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.permissions import AllowAny
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from django.views.generic import RedirectView
+from django.middleware.csrf import get_token
+from django.http import JsonResponse
+
+
+def csrf_token_view(request):
+    return JsonResponse({
+        "csrfToken": get_token(request)
+    })
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django Authentication API",
+        default_version='v1',
+        description="Cookie Based Authentication API",
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/', include('authentication.urls')),
+
+    path('csrf/', csrf_token_view),
+
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+
+    path('', RedirectView.as_view(url='/swagger/')),
 ]
